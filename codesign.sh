49351de5803f984526a0084ff26361a996e0a399
dacaiguoguo <dacaiguoguo@gmail.com>
2015-05-13 10:59:45 +0800

    codesign --display -r-

---
+++ codesign.sh
@@ -0,0 +1,3 @@
+codesign --display -r- Payload/test.app 
+Executable=/Users/me/Desktop/test/Payload/test.app/test
+designated => anchor apple generic and certificate leaf[field.1.2.840.113635.100.6.1.3] /* exists */ and identifier "com.test.test"
