18e0ade19080d58119b5ad9a38ba59f64d3d729d
dacaiguoguo <dacaiguoguo@gmail.com>
2015-05-15 10:27:43 +0800

    下载支付宝sdk

---
+++ downloadalipay.sh
@@ -0,0 +1,6 @@
+下载支付宝sdk
+
+https://b.alipay.com/order/productDetail.htm?productId=2014110308141993&tabId=4#ps-tabinfo-hash
+
+
+wget http://download.alipay.com/public/api/base/WS_MOBILE_PAY_SDK_BASE.zip
