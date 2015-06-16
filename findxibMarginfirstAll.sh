c583b2f3f9f8df742e0e77291c8f84938926a858
dacaiguoguo <dacaiguoguo@gmail.com>
2015-06-16 10:57:07 +0800

    findxibFirstBaselineAll add

--- findxibMarginfirstAll.sh
+++ findxibMarginfirstAll.sh
@@ -10,3 +10,18 @@ function findxibMarginfirstAll(){
 		fi
 	done
 }
+
+
+
+function findxibFirstBaselineAll(){ 
+	for file in `ls $1 | grep xib` 
+	do
+		find $1"/"$file |xargs grep "firstBaseline" && echo $1"/"$file && echo "--------------------end-------------\n"
+	done
+	for file in `ls $1` 
+	do
+		if [ -d $1"/"$file ]; then
+			findxibFirstBaselineAll $1"/"$file
+		fi
+	done
+}
